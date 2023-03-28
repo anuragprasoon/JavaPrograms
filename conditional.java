fadd9886213889a29288a8eb5f84a99566527b4d
anuragprasoon <anuragprasoon23@gmail.com>
2023-03-29 00:11:20 +0530

    learning Java

---
+++ conditional.java
@@ -0,0 +1,10 @@
+public class conditional {
+    public static void main(String args[]){
+        int a=10;
+        if(a<=10){
+            System.out.println("good");
+        }else{
+            System.out.println("bad");
+        }
+    }
+}
--- timeconverter.java
+++ timeconverter.java
@@ -1,4 +1,11 @@
 // convert days into sec
+import java.util.Scanner;
 public class timeconverter {
+    public static void main(String args[]){
+        Scanner x= new Scanner(System.in);
+        int day = x.nextInt();
+        int sec = 60*60*24*day;
+        System.out.println(sec);
+    }
     
 }
