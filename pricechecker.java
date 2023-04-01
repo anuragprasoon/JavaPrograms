a0db370615fcbbae5a6a11198e7aea95704f1527
anuragprasoon <anuragprasoon23@gmail.com>
2023-04-02 00:30:07 +0530

    Learning Java

---
+++ pricechecker.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class pricechecker {
+
+   public static void main(String[] args) {
+       
+       Scanner scanner = new Scanner(System.in);
+       int price = scanner.nextInt();
+       //complete the code
+       if(price<=12000){
+           System.out.println("yes");
+       }
+
+     
+   }
+}
