0a14e6bd0d22b63c21d95723111cd469721d965f
anuragprasoon <anuragprasoon23@gmail.com>
2023-04-03 02:34:49 +0530

    Learning Java

---
+++ sumjava.java
@@ -0,0 +1,11 @@
+public class sumjava
+{
+    public static void main(String[] args) {
+	int beer  = 64;
+        int whisky = 23;
+       //calculate the sum and output it
+       int sum = beer + whisky;
+       System.out.println(sum);
+        
+	}
+}
