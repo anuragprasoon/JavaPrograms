c893428e2f1ca4a16a1f78f4df9a6e713e42dde2
anuragprasoon <anuragprasoon23@gmail.com>
2023-04-04 00:16:57 +0530

    Learning Java

---
+++ welcomebot.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class welcomebot {
+
+   public static void main(String[] args) {
+       Scanner read = new Scanner(System.in);
+       int numberOfPeople = read.nextInt();
+       
+       for (int i = 0; i < numberOfPeople; i++) {
+           welcome();
+       }
+   }
+   public static void welcome() {
+       //complete the method
+       System.out.println("Welcome!");
+       
+   }
+}
