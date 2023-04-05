9a400bbfb97050e8577583cce4835aac118ab248
anuragprasoon <anuragprasoon23@gmail.com>
2023-04-06 00:59:21 +0530

    Learning Java

---
+++ fixthecalendar.java
@@ -0,0 +1,10 @@
+public class fixthecalendar {
+        public static void main(String[] args) {
+            String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+     
+            for (int i = 0; i < 7; i++) {
+                System.out.println(days[i]);
+            }
+        }
+}
+
